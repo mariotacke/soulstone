523efc98463d2fb3d1f7e838c4c3517e042eab35
Mario Tacke <tacke.mario@gmail.com>
2019-06-12 16:20:55 -0700

    Updates

---
+++ precache-manifest.f9703de9c044179a0e981b99ce63f8a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1aa0837694005e11c02af8a965e85e2",
+    "url": "/soulstone/index.html"
+  },
+  {
+    "revision": "c69d9a4439c5650de8ea",
+    "url": "/soulstone/static/css/main.493e5448.chunk.css"
+  },
+  {
+    "revision": "5547b7e4355b1b6076bb",
+    "url": "/soulstone/static/js/2.69f6af12.chunk.js"
+  },
+  {
+    "revision": "c69d9a4439c5650de8ea",
+    "url": "/soulstone/static/js/main.c53ef637.chunk.js"
+  },
+  {
+    "revision": "10cc1f7ca6d482148792",
+    "url": "/soulstone/static/js/runtime~main.74dd1bde.js"
+  }
+]);
